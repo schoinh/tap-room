0eb658352ffae2c6b171e3fb3d35c9cad765dd83
Na Hyung Choi <schoinh@gmail.com>
2019-09-06 14:23:21 -0700

    style buttons and add error 404 path

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -4,20 +4,53 @@ import { Link } from "react-router-dom";
 import logo from "../assets/images/Logo.png";
 
 function Splash() {
-  var splashStyle = {
+  var headerStyle = {
+    backgroundColor: "white"
+  };
+
+  var logoStyle = {
     marginRight: "auto",
     marginLeft: "auto",
     marginTop: "30px",
+    marginBottom: "30px",
     display: "block"
+  };
+
+  var buttonArea = {
+    paddingTop: "70px",
+  };
+
+  var menuButtonStyle = {
+    fontFamily: "Courgette, cursive",
+    display: "block",
+    color: "#b9e567",
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderWidth: "5px",
+    borderColor: "#b9e567",
+    padding: "20px",
+    fontSize: "36px"
+  };
+
+  var employeeButtonStyle = {
+    fontFamily: "Courgette, cursive",
+    display: "block",
+    color: "#b9e567",
+    margin: "auto",
+    borderWidth: "0px",
+    padding: "20px",
+    fontSize: "24px"
   };
 
   return (
     <div>
-      <div>
-        <img style={splashStyle} src={logo} />
+      <div style={headerStyle}>
+        <img style={logoStyle} src={logo} />
       </div>
-      <Link to="./menu"><button>Menu</button></Link>
-      <Link to="./employees"><button>Employees Only</button></Link>
+      <div style={buttonArea}>
+        <p><Link to="./menu"><button style={menuButtonStyle}>View Our Menu</button></Link></p>
+        <p><Link to="./employees"><button style={employeeButtonStyle}>Employees Only</button></Link></p>
+      </div>
     </div>
   );
 }
