6e0f8c8b5a362631475d87fa64efc7ad37670bcc
Na Hyung Choi <schoinh@gmail.com>
2019-09-06 14:19:39 -0700

    add style- and css-loaders to npm

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,14 @@ module.exports = {
             name: "images/[hash]-[name].[ext]"
           }
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
     ]
   },
 
